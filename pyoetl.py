16f8f6b8ebfb65e4b8e18c437e195250a804021b
Lev Givon <lev@columbia.edu>
2015-10-14 09:15:58 -0400

    Modify to facilitate programmatic invocation.

--- pyoetl.py
+++ pyoetl.py
@@ -10,51 +10,80 @@ Python wrapper for OrientDB ETL tool.
 # http://www.opensource.org/licenses/bsd-license
 
 import argparse
+import glob
 import os
 import subprocess
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('args', nargs=1)
-sys_args = parser.parse_args()
+class OETLProcessor(object):
+    """
+    OrientDB ETL Python wrapper.
+    """
 
-file_name = os.path.realpath(sys_args.args[0])
-if not os.environ.has_key('ORIENTDB_DIR'):
-    raise ValueError('ORIENTDB_DIR not defined')
-ORIENTDB_HOME = os.environ['ORIENTDB_DIR']
+    def __init__(self, ORIENTDB_DIR, JAVA='java', JAVA_OPTS=[]):
+        # Check if the specified OrientDB home directory actually contains the
+        # ETL JAR:
+        if not glob.glob(os.path.join(ORIENTDB_DIR, 'lib/orientdb-etl-*.jar')):
+            raise ValueError('invalid OrientDB home directory')
 
-JAVA = os.path.join(os.environ['JAVA_HOME'], 'bin/java')
-if not os.path.exists(JAVA):
-    JAVA = 'java'
-ORIENTDB_SETTINGS = ['-Djava.util.logging.config.file='+os.path.join(ORIENTDB_HOME,
-                     'config/orientdb-client-log.properties'),
-                     '-Djava.awt.headless=true']
-if os.environ.has_key('JAVA_OPTS'):
-    JAVA_OPTS = [os.environ['JAVA_OPTS']]
-else:
-    JAVA_OPTS = []
+        ORIENTDB_SETTINGS = ['-Djava.util.logging.config.file='+os.path.join(ORIENTDB_DIR,
+                             'config/orientdb-client-log.properties'),
+                             '-Djava.awt.headless=true']
 
-KEYSTORE = os.path.join(ORIENTDB_HOME, 'config/cert/orientdb-console.ks')
-KEYSTORE_PASS = 'password'
-TRUSTSTORE = os.path.join(ORIENTDB_HOME, 'config/cert/orientdb-console.ts')
-TRUSTSTORE_PASS = 'password'
-SSL_OPTS = ['-Xmx512m', '-Dclient.ssl.enabled=false', 
-            '-Djavax.net.ssl.keyStore=%s' % KEYSTORE,
-            '-Djavax.net.ssl.keyStorePassword=%s' % KEYSTORE_PASS,
-            '-Djavax.net.ssl.trustStore=%s' % TRUSTSTORE,
-            '-Djavax.net.ssl.trustStorePassword=%s' % TRUSTSTORE_PASS]
+        KEYSTORE = os.path.join(ORIENTDB_DIR, 'config/cert/orientdb-console.ks')
+        KEYSTORE_PASS = 'password'
+        TRUSTSTORE = os.path.join(ORIENTDB_DIR, 'config/cert/orientdb-console.ts')
+        TRUSTSTORE_PASS = 'password'
+        SSL_OPTS = ['-Xmx512m', '-Dclient.ssl.enabled=false', 
+                    '-Djavax.net.ssl.keyStore=%s' % KEYSTORE,
+                    '-Djavax.net.ssl.keyStorePassword=%s' % KEYSTORE_PASS,
+                    '-Djavax.net.ssl.trustStore=%s' % TRUSTSTORE,
+                    '-Djavax.net.ssl.trustStorePassword=%s' % TRUSTSTORE_PASS]
+        self._java_args = [JAVA, '-server']+JAVA_OPTS+ORIENTDB_SETTINGS+SSL_OPTS+\
+                          ['-Dfile.encoding=utf-8', '-Dorientdb.build.number="@BUILD@"', 
+                           '-cp', os.path.join(ORIENTDB_DIR, 'lib/*'),
+                           'com.orientechnologies.orient.etl.OETLProcessor']
 
-java_args = [JAVA, '-server']+JAVA_OPTS+ORIENTDB_SETTINGS+SSL_OPTS+\
-       ['-Dfile.encoding=utf-8', '-Dorientdb.build.number="@BUILD@"', 
-        '-cp', os.path.join(ORIENTDB_HOME, 'lib/*'),
-        'com.orientechnologies.orient.etl.OETLProcessor',
-        sys.argv[1]]
+    def process(self, file_name, out=False):
+        """
+        Process a file with OrientDB ETL. Display output if `out` is True.
+        Returns the execution return code.
+        """
 
-try:
-    ps = subprocess.Popen(java_args, stdout=subprocess.PIPE)
-    while ps.poll() is None:
-        l = ps.stdout.readline()
-        print l,
-    print ps.stdout.read()
-except Exception as e:
-    pass
+        if out:
+            ps = subprocess.Popen(self._java_args+[file_name],
+                                  stdout=subprocess.PIPE)
+            while ps.poll() is None:
+                l = ps.stdout.readline()
+                print l,
+            print ps.stdout.read()
+            return ps.returncode
+        else:
+            return subprocess.call(self._java_args)
+
+if __name__ == '__main__':        
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', help="Display command output.", action='store_true')
+    parser.add_argument('args', nargs=1)
+    args = parser.parse_args()
+
+    if not os.environ.has_key('ORIENTDB_DIR'):
+        raise ValueError('ORIENTDB_DIR not defined')
+    ORIENTDB_DIR = os.environ['ORIENTDB_DIR']
+
+    JAVA = os.path.join(os.environ['JAVA_HOME'], 'bin/java')
+    if not os.path.exists(JAVA):
+        JAVA = 'java'
+    if os.environ.has_key('JAVA_OPTS'):
+        JAVA_OPTS = [os.environ['JAVA_OPTS']]
+    else:
+        JAVA_OPTS = []
+
+    p = OETLProcessor(ORIENTDB_DIR, JAVA, JAVA_OPTS)
+    if args.d:
+        out = True
+    else:
+        out = False
+    for file_name in args.args:
+        p.process(os.path.realpath(file_name), out)
+
